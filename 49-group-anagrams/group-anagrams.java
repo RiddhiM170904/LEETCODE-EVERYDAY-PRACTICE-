125be63cfbaf6e74a889032d5c7b76d507b90447
Riddhi Mhadgut <riddhimhadgut17@gmail.com>
2026-07-23 12:11:48 +0530

    Time: 7 ms (73.75%) | Memory: 49.8 MB (33.74%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        //ek hashmap banaya h taki voh string aur usse valid esa valid anagram store krr skee
+        HashMap<String,List<String>> map = new HashMap<>(); 
+
+        for(String str : strs){//access kre elements froma string
+            char[] arr = str.toCharArray();//usko sort krne ke liye arr banaya 
+            Arrays.sort(arr);//sort kiya
+            String key = new String(arr);//usko vapas string mein convert kiya
+            if(!map.containsKey(key)){//check if in hashmap
+                map.put(key, new ArrayList<>());//if not nayi arraylist bana dii
+            }
+            map.get(key).add(str);//nhi toh direct add krr diya
+        }
+        return new ArrayList(map.values());//values vali arraylist return krr dii
+    }
+}
